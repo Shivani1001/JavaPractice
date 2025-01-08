63168f5cbd4845a611053f2e8f8f0e2221992171
Shivani1001 <67013024+Shivani1001@users.noreply.github.com>
2025-01-08 14:30:09 +0530

    Create matrix.java

---
+++ matrix.java
@@ -0,0 +1,18 @@
+public class PrintMatrixToConsole {
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+				{1, 2, 3, 4, 5,},
+				{6, 7},
+				{8, 9, 10}
+		};
+		
+		for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t"); // Print each element with a tab for spacing
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
+	}
+	
+}
