afb31e4b6aed0db73b2830d7865298c213627ba9
Shivani1001 <67013024+Shivani1001@users.noreply.github.com>
2025-01-08 14:29:24 +0530

    Create pyrimid.java

---
+++ pyrimid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PyramidInConsole {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		System.out.print("Please, enter height of the pyramid: ");
+		int height = scanner.nextInt();
+		for (int i= 1; i<= height; i++){
+		    for (int j=1; j<=i; j++){
+		    System.out.print("*");
+		        
+		    }
+		    System.out.println(); // Move to the next line
+		    
+		}
+        for (int i = height - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+    }
+
+}
